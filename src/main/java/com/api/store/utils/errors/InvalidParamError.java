fe013ce553a47c6f72c42fd2beac1ed132222806
willian <willian.cacoelho@gmail.com>
2024-03-25 11:45:20 -0300

    refactor: remove unused imports

--- src/main/java/com/api/store/utils/errors/InvalidParamError.java
+++ src/main/java/com/api/store/utils/errors/InvalidParamError.java
@@ -1,9 +1,5 @@
 package com.api.store.utils.errors;
 
-import org.springframework.http.HttpStatus;
-
-import java.util.List;
-
 public class InvalidParamError extends RuntimeException {
 
     public InvalidParamError(String field) {
