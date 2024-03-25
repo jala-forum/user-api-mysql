e21b2ed39328b97fc860e15f20c17e7784a2ac76
willian <willian.cacoelho@gmail.com>
2024-03-25 08:18:21 -0300

    feat: implements entity decorators in Sale entity

--- src/main/java/com/api/user/model/entities/mysql/Sale.java
+++ src/main/java/com/api/user/model/entities/mysql/Sale.java
@@ -1,8 +1,31 @@
 package com.api.user.model.entities.mysql;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UuidGenerator;
+
+import java.time.LocalDateTime;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "sale")
 public class Sale {
-    String id;
-    String ProductId;
-    String quantity;
-    String date;
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private String id;
+
+
+    private String ProductId;
+
+    private String quantity;
+
+    @Column(name = "created_at")
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
