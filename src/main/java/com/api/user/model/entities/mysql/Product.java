d08e1eb0fdffdd2c70aa20cef3f1e03e70b54fc4
willian <willian.cacoelho@gmail.com>
2024-03-25 08:13:57 -0300

    feat: implements entity decorators in Product entity

--- src/main/java/com/api/user/model/entities/mysql/Product.java
+++ src/main/java/com/api/user/model/entities/mysql/Product.java
@@ -1,8 +1,24 @@
 package com.api.user.model.entities.mysql;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.UuidGenerator;
+
+@Setter
+@Getter
+@Entity
+@Table(name = "product")
 public class Product {
-    String id;
-    String name;
-    int price;
-    int stock;
+    @Id
+    @UuidGenerator(style = UuidGenerator.Style.TIME)
+    private String id;
+
+    private String name;
+
+    private int price;
+
+    private int stock;
 }
