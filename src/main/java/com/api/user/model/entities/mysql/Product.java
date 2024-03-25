7d77dab2105203b2b401ffc2fb6be456d4f0e5ea
willian <willian.cacoelho@gmail.com>
2024-03-24 23:13:11 -0300

    feat: create Product and Sale entity

---
+++ src/main/java/com/api/user/model/entities/mysql/Product.java
@@ -0,0 +1,8 @@
+package com.api.user.model.entities.mysql;
+
+public class Product {
+    String id;
+    String name;
+    int price;
+    int stock;
+}
---
+++ src/main/java/com/api/user/model/entities/mysql/Sale.java
@@ -0,0 +1,8 @@
+package com.api.user.model.entities.mysql;
+
+public class Sale {
+    String id;
+    String ProductId;
+    String quantity;
+    String date;
+}
