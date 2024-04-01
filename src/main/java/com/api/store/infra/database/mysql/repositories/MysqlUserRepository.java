91af76c419b74cffad32457812ccffa8fdf7844f
willian <willian.cacoelho@gmail.com>
2024-04-01 19:24:52 -0300

    refactor: change MysqlProductRepository to MysqlUserRepository

--- src/main/java/com/api/store/infra/database/mysql/repositories/MysqlUserRepository.java
+++ src/main/java/com/api/store/infra/database/mysql/repositories/MysqlUserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface MysqlProductRepository extends JpaRepository<User, UUID> {
+public interface MysqlUserRepository extends JpaRepository<User, UUID> {
 }
--- src/main/java/com/api/store/service/UserService.java
+++ src/main/java/com/api/store/service/UserService.java
@@ -1,6 +1,6 @@
 package com.api.store.service;
 
-import com.api.store.infra.database.mysql.repositories.MysqlProductRepository;
+import com.api.store.infra.database.mysql.repositories.MysqlUserRepository;
 import com.api.store.model.entities.mysql.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,32 +11,32 @@ import java.util.UUID;
 
 @Service
 public class UserService {
-    private final MysqlProductRepository mysqlProductRepository;
+    private final MysqlUserRepository mysqlUserRepository;
 
     @Autowired
-    public UserService(MysqlProductRepository mysqlProductRepository) {
-        this.mysqlProductRepository = mysqlProductRepository;
+    public UserService(MysqlUserRepository mysqlUserRepository) {
+        this.mysqlUserRepository = mysqlUserRepository;
     }
 
     public void save(User product) {
-        this.mysqlProductRepository.save(product);
+        this.mysqlUserRepository.save(product);
     }
 
     public List<User> getAll() {
-        return this.mysqlProductRepository.findAll();
+        return this.mysqlUserRepository.findAll();
     }
 
     public User getById(String id) {
-        Optional<User> optionalProduct = this.mysqlProductRepository.findById(UUID.fromString(id));
+        Optional<User> optionalProduct = this.mysqlUserRepository.findById(UUID.fromString(id));
 
         return optionalProduct.orElse(null);
     }
 
     public void deleteById(String id) {
-        this.mysqlProductRepository.deleteById(UUID.fromString(id));
+        this.mysqlUserRepository.deleteById(UUID.fromString(id));
     }
 
     public void editById(User product) {
-        this.mysqlProductRepository.save(product);
+        this.mysqlUserRepository.save(product);
     }
 }
