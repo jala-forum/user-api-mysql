ed32e8f9e2d65a1365713d01011bfe5ddd4c1152
willian <willian.cacoelho@gmail.com>
2024-04-24 19:44:37 -0300

    refactor: remove unused imports

--- src/main/java/com/api/store/controller/IdeaController.java
+++ src/main/java/com/api/store/controller/IdeaController.java
@@ -8,7 +8,6 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Set;
 
 @RestController
